712aa35982f1febf0d179c65717367fa86948b1b
romi2002 <empoleom@gmail.com>
2024-09-02 19:08:46 -0600

    Fixed ISR bug

--- src/main.cpp
+++ src/main.cpp
@@ -161,8 +161,8 @@ void setup() {
     Serial1.begin(115200);
 #else
     //-- Initialized GPIO02 (Used for "Reset To Factory")
-    pinMode(GPIO02, INPUT_PULLUP);
-    attachInterrupt(GPIO02, reset_interrupt, FALLING);
+    // pinMode(GPIO02, INPUT_PULLUP);
+    // attachInterrupt(GPIO02, reset_interrupt, FALLING);
 #endif
     Logger.begin(2048);
 
